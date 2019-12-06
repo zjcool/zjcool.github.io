7ed9686cfe1ca68806395fa84f3dc04f1155e6dd
eden <280828919@qq.com>
2019-12-06 16:38:28 +0800

    Site updated: 2019-12-06 16:38:28

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,3 +1,3 @@
-// build time:Fri Dec 06 2019 16:31:45 GMT+0800 (中国标准时间)
+// build time:Fri Dec 06 2019 16:38:24 GMT+0800 (中国标准时间)
 $(function(){var a=$(".recent-post-item img");if(a.length===0){a=$("#post-content img")}for(var t=0;t<a.length;t++){var o=$('<a href="'+a[t].src+'" data-fancybox="group" data-caption="'+a[t].alt+'" class="fancybox"></a>');var e=a[t].alt;var n=$(a[t]).wrap(o);if(e){n.after('<div class="img-alt">'+e+"</div>")}}$().fancybox({selector:"[data-fancybox]",loop:true,transitionEffect:"slide",buttons:["share","slideShow","fullScreen","download","thumbs","close"]});var r=$(".gallery-item");var i=[];r.each(function(a,t){i.push({src:$(t).data("url"),opts:{caption:$(t).data("title")}})});r.on("click",function(){$.fancybox.open(i,{loop:true,transitionEffect:"slide"},r.index(this));return false})});
 //rebuild by neat 
--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Fri Dec 06 2019 16:31:46 GMT+0800 (中国标准时间)
+// build time:Fri Dec 06 2019 16:38:24 GMT+0800 (中国标准时间)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
