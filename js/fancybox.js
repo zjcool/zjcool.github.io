789ec94c26c6792f9208222550cee5abf2ea6a36
eden <280828919@qq.com>
2019-12-18 05:54:59 +0800

    Site updated: 2019-12-18 05:54:59

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 18 2019 04:27:09 GMT+0800 (中国标准时间)
+// build time:Wed Dec 18 2019 05:54:52 GMT+0800 (中国标准时间)
 $(function(){var a=$(".recent-post-item img");if(a.length===0){a=$("#post-content img")}for(var t=0;t<a.length;t++){var o=$('<a href="'+a[t].src+'" data-fancybox="group" data-caption="'+a[t].alt+'" class="fancybox"></a>');var e=a[t].alt;var n=$(a[t]).wrap(o);if(e){n.after('<div class="img-alt">'+e+"</div>")}}$().fancybox({selector:"[data-fancybox]",loop:true,transitionEffect:"slide",buttons:["share","slideShow","fullScreen","download","thumbs","close"]});var r=$(".gallery-item");var i=[];r.each(function(a,t){i.push({src:$(t).data("url"),opts:{caption:$(t).data("title")}})});r.on("click",function(){$.fancybox.open(i,{loop:true,transitionEffect:"slide"},r.index(this));return false})});
 //rebuild by neat 
--- js/hexo-theme-melody.js
+++ js/hexo-theme-melody.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 18 2019 04:27:09 GMT+0800 (中国标准时间)
+// build time:Wed Dec 18 2019 05:54:52 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
