35c4234bc5a71cc3bac84e39080bd1e97c91ae5f
eden <280828919@qq.com>
2019-12-19 00:32:08 +0800

    Site updated: 2019-12-19 00:32:07

--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 18 2019 05:54:52 GMT+0800 (中国标准时间)
+// build time:Thu Dec 19 2019 00:32:04 GMT+0800 (中国标准时间)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
