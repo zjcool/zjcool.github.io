9b66c6dd8e0ecdb3b343015caf0dbfe123baa0e9
eden <280828919@qq.com>
2019-12-19 15:05:35 +0800

    Site updated: 2019-12-19 15:05:34

--- js/head.js
+++ js/head.js
@@ -1,3 +1,3 @@
-// build time:Thu Dec 19 2019 14:59:26 GMT+0800 (中国标准时间)
+// build time:Thu Dec 19 2019 15:05:30 GMT+0800 (中国标准时间)
 $(function(){$(".toggle-menu").on("click",function(){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}else{$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(document).on("click",function(i){var n=$(".menus")[0].contains(i.target)||$(".toggle-menu")[0].contains(i.target);if(!n&&$(".toggle-menu").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(window).on("resize",function(i){if(!$(".toggle-menu").is(":visible")){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}}})});
 //rebuild by neat 
