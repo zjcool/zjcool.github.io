316a32dcff56457ba770a6e6054e32144e1baa6a
eden <280828919@qq.com>
2020-01-08 16:07:53 +0800

    Site updated: 2020-01-08 16:07:52

--- js/head.js
+++ js/head.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 31 2019 09:49:12 GMT+0800 (中国标准时间)
+// build time:Wed Jan 08 2020 16:07:48 GMT+0800 (中国标准时间)
 $(function(){$(".toggle-menu").on("click",function(){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}else{$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(document).on("click",function(i){var n=$(".menus")[0].contains(i.target)||$(".toggle-menu")[0].contains(i.target);if(!n&&$(".toggle-menu").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideUpOut",{duration:300})}});$(window).on("resize",function(i){if(!$(".toggle-menu").is(":visible")){if(!$(".menus").is(":visible")){$(".menus").velocity("stop").velocity("transition.slideDownIn",{duration:300})}}})});
 //rebuild by neat 
--- js/hexo-theme-melody.js
+++ js/hexo-theme-melody.js
@@ -1,3 +1,3 @@
-// build time:Tue Dec 31 2019 09:49:12 GMT+0800 (中国标准时间)
+// build time:Wed Jan 08 2020 16:07:48 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
