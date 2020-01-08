4917eba986d73792ecbe2ea2a03f8741ad621511
eden <280828919@qq.com>
2020-01-08 16:16:38 +0800

    Site updated: 2020-01-08 16:16:38

--- js/hexo-theme-melody.js
+++ js/hexo-theme-melody.js
@@ -1,3 +1,3 @@
-// build time:Wed Jan 08 2020 16:07:48 GMT+0800 (中国标准时间)
+// build time:Wed Jan 08 2020 16:16:35 GMT+0800 (中国标准时间)
 
 //rebuild by neat 
--- js/transition.js
+++ js/transition.js
@@ -1,3 +1,3 @@
-// build time:Wed Jan 08 2020 16:07:48 GMT+0800 (中国标准时间)
+// build time:Wed Jan 08 2020 16:16:35 GMT+0800 (中国标准时间)
 $(function(){$(".layout").velocity("stop").velocity("transition.slideUpIn",{delay:500,duration:1e3,easing:"easeInOutQuart",complete:function(){if($(".sidebar-toc").length>0){setTimeout(function(){$("#toggle-sidebar").click()},200)}}});$("#top-container").velocity("stop").velocity("transition.fadeIn",{delay:500,duration:1e3,easing:"easeInOutQuart"})});
 //rebuild by neat 
